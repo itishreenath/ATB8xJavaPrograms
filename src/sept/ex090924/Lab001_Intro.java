20fe45b05fb05501ce47c5170cadaf2cf7a4607e
itishreenath <nathitishree9496@gmail.com>
2024-09-18 00:39:21 +0530

    Push for 060924

--- src/sept/ex090924/Lab001_Intro.java
+++
@@ -1,7 +0,0 @@
-package sept.ex090924;
-
-public class Lab001_Intro {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-    }
-}
--- src/sept/ex090924/Lab002_Intro2.java
+++
@@ -1,4 +0,0 @@
-package sept.ex090924;
-
-public class Lab002_Intro2 {
-}
