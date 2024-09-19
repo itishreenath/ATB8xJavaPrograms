952c822d483e87e229a1fb9b7a44590af9f13ff9
itishreenath <nathitishree9496@gmail.com>
2024-09-20 00:13:54 +0530

    Rename Task01 package -> Task

---
+++ src/sept/Task/task001_130924.java
@@ -0,0 +1,46 @@
+package sept.Task;
+
+public class task001_130924 {
+    public static void main(String[] args) {
+        /* Q1 - Write a program to print the table of number which user will give( int num 10)
+           output will be like this (by using the printf() and println() )
+           10 * 1 = 10
+           10 * 2 = 20
+           10 * 3 = 30….
+           10 * 10 = 100
+         */
+        int num = 10;
+        System.out.printf("%d * %d = %d",num,1,num*1);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,2,num*2);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,3,num*3);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,4,num*4);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,5,num*5);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,6,num*6);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,7,num*7);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,8,num*8);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,9,num*9);
+        System.out.println();
+        System.out.printf("%d * %d = %d",num,10,num*10);
+        System.out.println();
+
+        /* Q2 - Prepare a small calculator, which supports +, - , , / and %(?) - Modulus operator
+           double a = 34;
+           double b = 10;
+           Print - a+b, a-b, ab, a/b, a%b all the outputs.
+         */
+        double a = 34, b = 10;
+        System.out.println("Addition result = "+(a+b));
+        System.out.println("Subtraction result = "+(a-b));
+        System.out.println("Multiplication result = "+(a*b));
+        System.out.println("Division result = "+(a/b));
+        System.out.println("Modulus result = "+(a%b));
+    }
+}
---
+++ src/sept/Task/task002_160924.java
@@ -0,0 +1,37 @@
+package sept.Task;
+
+public class task002_160924 {
+    public static void main(String[] args) {
+        /* Q1: Difference between = and  == ?
+           Ans:a - '=' is an unary operator which is used to assign vaule to a single operand or variable.
+                 - This is know as Assignment Operator
+               b - '==' is a binary operator which compare the value of two operand or variable and returns a boolean value
+                 - This is known as Comparison Operator
+         */
+
+        /* Q2: byte b = 10; long l = 10l; → How much Byte will be used?
+           Ans: byte b = 10; -> 1 byte in the memory is used for this variable
+                long l = 10; -> 8 byte in the memory is used for this variable
+                Total memory used for this two variable = 9 byte = 72 bits.
+         */
+
+        /* Q3: Another byte b = 10; byte c = 10;  What is the output of b+c and what is the data type it will give in result?
+           Ans: Output = 20, result datatype = int (byte + byte = int)
+         */
+        byte b = 10,c = 10;
+        System.out.println("Result of b+c = "+(b+c));
+
+        /* Q4: Find the output for the following:
+               short s = 10;
+               char c = 'A';
+               int ss = s+c;
+               System.out.println(ss);
+           Ans: Output = 75
+         */
+        short s = 10;
+        char ch = 'A';
+        int ss = s+ch;
+        System.out.println("Result of 10 + A = "+ss);
+
+    }
+}
