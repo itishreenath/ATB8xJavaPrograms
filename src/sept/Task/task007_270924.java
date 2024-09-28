b3fc33a42cac6876d9c109993f8a345260a263fe
itishreenath <nathitishree9496@gmail.com>
2024-09-28 17:39:35 +0530

    changes for 27/09/24

---
+++ src/sept/Task/task007_270924.java
@@ -0,0 +1,16 @@
+package sept.Task;
+
+import java.util.Scanner;
+
+public class task007_270924 {
+    public static void main(String[] args) {
+        /* Q1: Give me example of while vs do while loop in Lab ex.
+         */
+        System.out.printf("Ans1: ");
+        Scanner sc = new Scanner(System.in); // New Object
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+    }
+}
